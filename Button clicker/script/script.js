0c155e982fb794b61022d1b1568c52dca6e1c646
erabullari <era1bullari1@gmail.com>
2023-12-07 19:07:20 +0100

    f

---
+++ Button clicker/script/script.js
@@ -0,0 +1,19 @@
+function accesing(element){
+    if (element.innerText =='Login'){
+       element.innerText ='Logout'
+    }
+    else{
+        
+        element.innerText ='Login'
+    }
+}
+
+function likes(){
+    alert('Ninja was liked')
+}
+
+
+ function deleted(){
+    greenbutton.remove();
+ }
+
