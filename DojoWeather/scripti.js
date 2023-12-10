c36129da939e39b3bb2639f5d9d5a7ef94c7a7f0
erabullari <era1bullari1@gmail.com>
2023-12-10 19:05:20 +0100

    DojoWeather updated

---
+++ DojoWeather/scripti.js
@@ -0,0 +1,36 @@
+function removeCookie(element){
+    document.getElementById('cookie').remove();
+}
+function message(element){
+    alert('Loading wather report')
+}
+
+
+
+function toFehratOnChange(selectedOption) {
+    // Check if the selected option is 'F'
+    if (selectedOption === 'F') {
+        // aksesoj temperaturen nepermjet clasave
+        //per cdo loop qe behet ndryshon nje klase,i perfaqson indeksin e klases ,
+        var kuqeElements = document.getElementsByClassName('kuqe');
+        for (var i = 0; i < kuqeElements.length; i++) {             
+            var numriGradeve = parseInt(kuqeElements[i].innerHTML);  //aksesohen klasat nepermjet indeksit 
+            var numriFefrat = Math.round((numriGradeve * 1.8) + 32);
+            kuqeElements[i].innerHTML = numriFefrat;
+        }
+    }
+    // Check if the selected option is 'C'
+    else if (selectedOption=== 'C'){
+        var kuqeElements = document.getElementsByClassName('kuqe');
+        for (var i = 0; i < kuqeElements.length; i++) {
+            var numriGradeve = parseInt(kuqeElements[i].innerHTML);
+            var numriFefrat = Math.round((numriGradeve -32) /1.8);
+            kuqeElements[i].innerHTML = numriFefrat;
+        }
+    }
+}
+
+
+
+
+
